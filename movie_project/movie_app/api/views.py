69cb4d1663d5fb55cdef3999fb21310f446cf1f1
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-21 11:54:48 +0100

    feat: movie search

--- movie_project/movie_app/api/views.py
+++ movie_project/movie_app/api/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics
 from ..models import Movie
 from .serializers import MovieSerializer
 from .pagination import MovieListPagination
+from rest_framework import filters
 
 class MovieListCreate(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     pagination_class = MovieListPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()

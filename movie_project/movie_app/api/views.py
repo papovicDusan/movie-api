743acca9ca4d34a07db27ad5c115e965d84d809b
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-21 01:04:31 +0100

    feat: movie pagination

--- movie_project/movie_app/api/views.py
+++ movie_project/movie_app/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
 from ..models import Movie
 from .serializers import MovieSerializer
+from .pagination import MovieListPagination
 
 class MovieListCreate(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
+    pagination_class = MovieListPagination
 
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()

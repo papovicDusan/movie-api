aa7a5716a4dd75f4979f7c6f19e9ca0a0567a9b1
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-23 00:41:06 +0100

    feat: ability to like/dislike movie

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 from .utils import MOVIE_GENRES
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Like(models.IntegerChoices):
+    LIKE = 1
+    DISLIKE = -1
+
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
     image_url = models.TextField()
     genre = models.CharField(max_length=20, choices=MOVIE_GENRES, default=MOVIE_GENRES.historical)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    is_liked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+class MovieLike(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_likes')
+    like = models.IntegerField(choices=Like.choices)
+
+    def __str__(self):
+        return f'{self.movie} | {self.user}'
--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated']
+    # 'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated']
 }
 
 SIMPLE_JWT = {

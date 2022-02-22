754fcf3f188ab87d9baeafd03c4f4dab52d267c9
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-23 00:41:06 +0100

    feat: ability to like/dislike movie

--- movie_project/movie_app/api/serializers.py
+++ movie_project/movie_app/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from ..models import Movie
+from ..models import Movie, MovieLike
+
+class MovieLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieLike
+        fields = ['like']
 
 class MovieSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'image_url', 'genre']
+        fields = ['id', 'title', 'description', 'image_url', 'genre', 'likes', 'dislikes', 'is_liked', ]
--- movie_project/movie_app/api/urls.py
+++ movie_project/movie_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovieListCreate, MovieDetail
+from .views import MovieListCreate, MovieDetail, LikeCreate
 
 urlpatterns = [
-    path('', MovieListCreate.as_view()),
-    path('<int:pk>/', MovieDetail.as_view())
+    path('', MovieListCreate.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetail.as_view(), name='movie-detail'),
+    path('likes/<int:pk>/like-create/', LikeCreate.as_view(), name='like-create'),
 ]

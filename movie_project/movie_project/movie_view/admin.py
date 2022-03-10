92588d4853d00c981281afd24c633171f87db849
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-03-10 11:12:03 +0100

    feat: upload image

--- movie_project/movie_project/movie_view/admin.py
+++ movie_project/movie_project/movie_view/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Reaction, Comment, MovieWatchlist
+from .models import Movie, Reaction, Comment, MovieWatchlist, MovieImage
 
 admin.site.register(Movie)
 admin.site.register(Reaction)
 admin.site.register(Comment)
 admin.site.register(MovieWatchlist)
+admin.site.register(MovieImage)
--- movie_project/movie_project/movie_view/models.py
+++ movie_project/movie_project/movie_view/models.py
@@ -1,22 +1,33 @@
 from django.db import models
 from .utils import MOVIE_GENRES
 from django.contrib.auth import get_user_model
+from easy_thumbnails.fields import ThumbnailerImageField
 
 User = get_user_model()
+
+
+class MovieImage(models.Model):
+    thumbnail = ThumbnailerImageField(
+        upload_to='static/thumbnails/', blank=True, null=True, resize_source=dict(size=(200, 200)))
+    full_size = ThumbnailerImageField(
+        upload_to='static/full-size/', blank=True, null=True, resize_source=dict(size=(400, 400)))
+
 
 class Like(models.IntegerChoices):
     LIKE = 1
     DISLIKE = -1
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
-    image_url = models.TextField()
+    image_url = models.OneToOneField(MovieImage, on_delete=models.CASCADE, blank=True, null=True)
     genre = models.CharField(max_length=20, choices=MOVIE_GENRES, default=MOVIE_GENRES.historical)
     visits = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.title
+
 
 class Reaction(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_likes')
@@ -29,6 +40,7 @@ class Reaction(models.Model):
     def __str__(self):
         return f'{self.movie} | {self.user}'
 
+
 class Comment(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
@@ -37,6 +49,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.movie} | {self.user}'
+
 
 class MovieWatchlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_watchlist')
